55fcda426c92bf92282667986a236627462ffcad
seregka23 <62717882+seregka23@users.noreply.github.com>
2024-07-17 16:00:48 +0000

    Add bot

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,9 +1,18 @@
+import { ConfigService } from "@nestjs/config";
 import { Ctx, Message, On, Start, Update } from "nestjs-telegraf";
+import { ChatgptService } from "src/chatgpt/chatgpt.service";
 import { Context, Telegraf } from 'telegraf'
 import { SceneContext } from 'telegraf/scenes'
 
 @Update()
 export class TelegramService extends Telegraf<SceneContext> {
+
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly gptService: ChatgptService
+    ) {
+        super(configService.get('TELEGRAM_API'));
+    }
 
     @Start()
     onStart(@Ctx() ctx: SceneContext) {
@@ -14,8 +23,11 @@ export class TelegramService extends Telegraf<SceneContext> {
 
     @On('text')
     onMessage(@Message('text') message: string, @Ctx() ctx: SceneContext) {
-        ctx.replyWithHTML(
-            `<i>${message}</i>`
-        );
+        
+        return this.gptService.generateResponse(message);
+        
+        // ctx.replyWithHTML(
+        //     `<i>${message}</i>`
+        // );
     }
 }
