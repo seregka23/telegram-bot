97ba8b82cfeb0981e9df32700bc8f9ac56bcc7a9
seregka23 <62717882+seregka23@users.noreply.github.com>
2024-07-17 13:02:33 +0000

    Init telegram and gpt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ChatgptModule } from './chatgpt/chatgpt.module';
+import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ChatgptModule, TelegramModule],
+  providers: [],
 })
 export class AppModule {}
